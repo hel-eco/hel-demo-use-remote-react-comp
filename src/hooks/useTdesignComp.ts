db2de3cdcf5dfd2e816ba49df7b8cbc37054dc88
fantasticsoul <zhongzhengkai@gmail.com>
2023-05-03 19:47:52 +0800

    feat: bump to v4.7

--- src/hooks/useTdesignComp.ts
+++ src/hooks/useTdesignComp.ts
@@ -3,10 +3,13 @@ import { Lib } from 'hel-tdesign-react';
 import tdesignCssVar from './tdesignCssVar';
 
 export default function useTdesignComp<Name extends keyof Lib>(compName: Name): Lib[Name] {
-  const { Comp, getSubVal } = useRemoteCompAndSubVal('hel-tdesign-react', compName, {
-    shadow: true,
-    // tdesign 有一些自己的css变量定义，这些定义未参与css打包，此处人工补偿一下
-    handleStyleStr: (str: string) => `${tdesignCssVar} ${str}`,
+  const { Comp, getSubVal } = useRemoteCompAndSubVal('remote-tdesign-react', compName, {
+    shadow: true, cssListToStr: false,
+    // tgesign 有一些自己的css变量定义，这些定义未参与css打包，此处人工补偿一下
+    onStyleFetched: ({ renderStyleStr }) => {
+      return `${tdesignCssVar} ${renderStyleStr}`;
+    },
+    // extraStyleStr: tdesignCssVar,
     ignoreHelContext: true,
   });
   // 补偿 TabPanel 下挂载的子组件对象，因Comp 是一个经shadow 包裹后的组件，取一些子组件是取不到的
